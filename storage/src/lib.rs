8e127db716af5bd604a83fbaf079d478dfe6c6b8
rooooooooob <raftias@gmail.com>
2019-05-23 10:23:09 -0700

    rust fmt

--- storage/src/lib.rs
+++ storage/src/lib.rs
@@ -442,7 +442,11 @@ impl Storage {
         self.chain_height_idx.packed_idx.len()
     }
 
-    pub fn loose_index_drop_from_head(&mut self, len: usize, delete_dropped_blobs: bool) -> Result<()> {
+    pub fn loose_index_drop_from_head(
+        &mut self,
+        len: usize,
+        delete_dropped_blobs: bool,
+    ) -> Result<()> {
         let read_idx = self.chain_height_idx.loose_idx.clone();
         let size = read_idx.len();
         if size < len {
